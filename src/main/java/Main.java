fbcb5a7fa2faa7c3a048b84f4341465bfa7c88ba
engsheng <angengsheng2803@gmail.com>
2026-09-04 18:15:02 +0800

    Refactor DialogBox creation and add flip functionality for dialog alignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -86,12 +86,12 @@ public class Main extends Application {
      * Creates a dialog box containing user input, and appends it to
      * the dialog container. Clears the user input after processing.
      */
-   private void handleUserInput() {
+    private void handleUserInput() {
         String userText = userInput.getText();
         String dukeText = duke.getResponse(userInput.getText());
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, userImage),
-                new DialogBox(dukeText, dukeImage)
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage)
         );
         userInput.clear();
     }
